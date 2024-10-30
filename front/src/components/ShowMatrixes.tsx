fae9e094a18db4ecceafeeb873dea8f31a9fe71e
josemigg <josemiggomez16@gmail.com>
2024-10-30 01:13:19 +0100

    Add pagination to equivalentes

--- front/src/components/ShowMatrixes.tsx
+++ front/src/components/ShowMatrixes.tsx
@@ -1,10 +1,13 @@
 import { matrixStore } from '@/stores/MatrixStore'
 import { useStore } from '@nanostores/preact'
-import { useEffect } from 'preact/hooks'
+import { useEffect, useState } from 'preact/hooks'
 import MatrixComponent from './MatrixComponent'
+import Equivalentes from './Equivalentes'
+
 
 const ShowMatrixes = () => {
 	const MatrixState = useStore(matrixStore)
+
 	console.log(MatrixState)
 	useEffect(() => {
 		MatrixState.map((a) => console.log(a))
@@ -14,26 +17,23 @@ const ShowMatrixes = () => {
 			{MatrixState.toReversed().map((matrix, i) => (
 				<li class='relative flex flex-col bg-slate-700 px-8 py-2'>
 					<button
-					onClick={() => {
-						const equivalentes = document.querySelector(`.equivalentes-${i}`)
-						equivalentes?.classList.toggle('hidden')
-					}}
-					 class='absolute bg-slate-800 border-white border-2 px-2 text-white rounded top-0 right-0 m-2'>V</button>
+						onClick={() => {
+							const equivalentes = document.querySelector(`.equivalentes-${i}`)
+							equivalentes?.classList.toggle('hidden')
+						}}
+						class='absolute right-0 top-0 m-2 rounded border-2 border-white bg-slate-800 px-2 text-white'
+					>
+						V
+					</button>
 					<div class='flex'>
 						<MatrixComponent matrix={matrix.Original} operacion={matrix.operacion} />
 						<p class='mr-4 flex items-center text-7xl'>{'='}</p>
 						<MatrixComponent matrix={matrix.Inversa} />
 					</div>
+
 					<ul class={`equivalentes-${i} mt-5 hidden`}>
-						{matrix.Equivalentes.map((equivalente, i) => (
-							<li class={`border-t-2 border-gray-500 py-8 }`} >
-								<h3 class='mb-8'>{equivalente.text}</h3>
-								<div class='flex items-center'>
-								<p class='text-4xl font-bold'>E{i + 1}=</p>
-								<MatrixComponent  matrix={equivalente.matriz} />
-								</div>
-							</li>
-						))}
+						{/* Pagina solo una parte de los equivalentes */}
+						<Equivalentes matrix={matrix} />
 					</ul>
 				</li>
 			))}
