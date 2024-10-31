bba610c7da0d56d4810324b1dda48cae14537a5b
Chrsitian <christianmendozater@Gmail.com>
2024-10-31 19:07:12 -0400

    import gauss method in matrixxx, and validate matrix in inverse

--- flaskr/objects/matrixxx.py
+++ flaskr/objects/matrixxx.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 #importing methods
-from objects.methods.reverse import inversa_matriz
+from objects.methods.gaussJordan import gauss_jordan
 from objects.methods.lu import lu_factorizacion
 
 class Matrixxx:
@@ -17,11 +17,18 @@ class Matrixxx:
         return data
 
     def inverse(self):
-        data = inversa_matriz(self.matriz)
+        # Verificar que la matriz sea cuadrada
+        if self.matriz.shape[0] != self.matriz.shape[1]:
+            raise ValueError("La matriz debe ser cuadrada para calcular su inversa.")
+        
+        # Verificar que la determinante sea diferente de 0
+        if np.linalg.det(self.matriz) == 0:
+            raise ValueError("La matriz no tiene inversa porque su determinante es 0.")
+        
+        # Si pasa las validaciones, usar el método de Gauss-Jordan
+        data = gauss_jordan(self.matriz)
         return data
-    
+
     def lu(self):
         data = lu_factorizacion(self.matriz)
         return data
-    
-
