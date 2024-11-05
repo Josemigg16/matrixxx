b01c2100c24473d94377b3a3e5eba88faea39a35
josemigg <josemiggomez16@gmail.com>
2024-11-05 04:41:29 +0100

    make array generic

--- front/src/stores/MatrixStore.ts
+++ front/src/stores/MatrixStore.ts
@@ -1,16 +1,26 @@
-import { atom } from "nanostores";
+import { atom } from 'nanostores'
 
-export interface OPMatrix {
-    Original: string[][],
-    Aumentada: string[][],
-    Equivalentes: Equivalentes[],
-    Inversa: string[][]
-    operacion: string
+export interface OPInversa {
+	Original: string[][]
+	Aumentada: string[][]
+	Equivalentes: Equivalentes[]
+	Inversa: string[][]
+	operacion: string
+}
+export interface OPLU {
+	Original: string[][]
+	Equivalentes: Equivalentes[]
+	L: string[][]
+	U: string[][]
+	operacion: string
 }
 
 interface Equivalentes {
-    text: string,
-    matriz: string[][]
+	text: string
+	matriz?: string[][]
+	L?: string[][]
+	U?: string[][]
+	P?: string[][]
 }
 
-export const matrixStore = atom<OPMatrix[]>([])
+export const matrixStore = atom<never[]>([])
